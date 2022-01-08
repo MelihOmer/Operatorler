cc7a0de8b9d9fc63b5f6beb7818bd41beef6ad7a
MelihOmer <97173932+MelihOmer@users.noreply.github.com>
2022-01-09 01:44:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operatörler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x =3;
+            int y = 3;
+            y = y+2;
+            y +=2;
+            y/=1;
+            x *=2;'
+        }
+    }
+}
